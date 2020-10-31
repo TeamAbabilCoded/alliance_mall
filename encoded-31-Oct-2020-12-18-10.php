7a4b99c50ca6cd8e00b0f872ff962825d3af9e35
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2020-11-01 01:14:56 +0700

    Update encoded-31-Oct-2020-12-18-10.php

--- encoded-31-Oct-2020-12-18-10.php
+++ encoded-31-Oct-2020-12-18-10.php
@@ -1,16 +1,5 @@
 <?php
-$biru="\033[1;34m";
-$turkis="\033[1;36m";
-$ijo="\033[92m";
-$putih="\033[1;37m";
-$pink="\033[1;35m";
-$red="\033[1;31m";
-$kuning="\033[1;33m";
-$t = "\n";
-$r = "\n\n";
+
 @system("clear");
-echo$kuning."[•] Silahkan Subrek dulu ".$t;
-echo$kuning."[•] Link Yt : https://www.youtube.com/channel/UCmYgwOqIVPUjj8Kmpf2WF8Q
-ob_start();
-exec("https://www.youtube.com/channel/UCmYgwOqIVPUjj8Kmpf2WF8Q");
-ob_end_clean();
+echo"Silahkan Subrek dulu ".$t;
+echo"ink Yt : https://www.youtube.com/channel/UCmYgwOqIVPUjj8Kmpf2WF8Q

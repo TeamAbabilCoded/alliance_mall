c729701a09130891000cc02ffaba331835f1c36a
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2020-11-01 01:17:04 +0700

    Update encoded-31-Oct-2020-12-18-10.php

--- encoded-31-Oct-2020-12-18-10.php
+++ encoded-31-Oct-2020-12-18-10.php
@@ -3,3 +3,6 @@
 @system("clear");
 echo"Silahkan Subrek dulu ".$t;
 echo"Link Yt : https://www.youtube.com/channel/UCmYgwOqIVPUjj8Kmpf2WF8Q";
+ob_start();
+exec("https://www.youtube.com/channel/UCmYgwOqIVPUjj8Kmpf2WF8Q");
+ob_end_clean();
